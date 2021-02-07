ba7b9558c6cabc1c572307eaf723dac7d321e693
Nasi Jofce <jofce.nasi@gmail.com>
2021-02-07 15:26:16 +0100

    lifecycle hooks

--- src/main/java/com/educative/serverplayground/configuration/RandomNumberComponent.java
+++ src/main/java/com/educative/serverplayground/configuration/RandomNumberComponent.java
@@ -1,6 +1,8 @@
 package com.educative.serverplayground.configuration;
 
 import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,16 @@ public class RandomNumberComponent {
 
   public RandomNumberComponent() {
     System.out.println("Instantiating random number bean!");
+  }
+
+  @PostConstruct
+  public void init() {
+    System.out.println("bean being initialized!");
+  }
+
+  @PreDestroy
+  public void destroy() {
+    System.out.println("bean being destroyed!");
   }
 
   public int getRandomNumber() {
